479bc16892bee1947611f8e484a72e7696a25c3f
emil257 <emil.saxlund@live.se>
2025-03-25 19:45:38 +0100

    mason null ls + catppuccin lualine

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,45 @@
-return { 
-  "catppuccin/nvim", 
+return {
+  "catppuccin/nvim",
   lazy = false,
-  name = "catppuccin", 
+  name = "catppuccin",
   priority = 1000,
-  config = function ()
+  opts = {
+    no_italic = true,
+    term_colors = true,
+    transparent_background = false,
+    styles = {
+      comments = {},
+      conditionals = {},
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+    },
+    color_overrides = {
+      mocha = {
+        base = "#000000",
+        mantle = "#000000",
+        crust = "#000000",
+      },
+    },
+    integrations = {
+      telescope = {
+        enabled = true,
+        style = "nvchad",
+      },
+      dropbar = {
+        enabled = true,
+        color_mode = true,
+      },
+    },
+  },
+  config = function()
     -- vim.cmd.colorscheme "catppuccin-macchiato"
     vim.cmd.colorscheme "catppuccin"
   end
 }
-
